fb88c9300fc23d2893a4a469171622c4bbf9d25c
TNBT <tiennguyen12g@gmail.com>
2023-11-07 22:04:36 +0700

    delete empty file

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import PinLockScreen from './PinLockScreen';
 import MainScreen from './MainScreen';
 import NumberKeyBoard from './NumberKeyBoard';
-import Test from './Test';
 
 const Stack = createStackNavigator();
 
@@ -12,10 +10,8 @@ function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="NumberKeyBoard">
-        {/* <Stack.Screen name="PinLockScreen" component={PinLockScreen} /> */}
         <Stack.Screen name="NumberKeyBoard" component={NumberKeyBoard} />
         <Stack.Screen name="MainScreen" component={MainScreen} />
-        <Stack.Screen name="Test" component={Test} />
       </Stack.Navigator>
     </NavigationContainer>
   );
